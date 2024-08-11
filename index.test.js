be83e6e64e843aa4d2c59618f5dfad4b29d0c583
nicauryS <119086333+nicauryS@users.noreply.github.com>
2024-08-11 16:55:37 -0400

    Add files via upload

---
+++ index.test.js
@@ -0,0 +1,7 @@
+// index.test.js
+const fs = require('fs');
+
+test('El archivo index.html contiene "Hola Mundo"', () => {
+    const html = fs.readFileSync('./index.html', 'utf8');
+    expect(html).toContain('Hola Mundo');
+});
